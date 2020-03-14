a14de7ff422d3a10dcee0996b3c213a4b8169abb
Rapunzel <Laurenminton19@yahoo.com>
2020-03-14 19:09:34 -0400

    adding routes

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+// Create express server
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Point to server routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Set up DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true
+});
+
+// Start server
+app.listen(PORT, function() {
+  console.log(`Now listening on port ${PORT} at http://localhost:${PORT}`);
+});
