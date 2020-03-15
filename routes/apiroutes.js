7c3cf3a7e33288e567c72ea79d13f67df4fb4fbd
Rapunzel <Laurenminton19@yahoo.com>
2020-03-15 14:19:44 -0400

    still having bugs and connection issues

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -9,19 +9,19 @@ router.post("/api/workouts", (req, res) => {
         .catch(err => {
             res.json(err)
         })
-})
+});
 
 router.get("/api/workouts", (req, res) => {
-    Workout.find({})
+    Workout.find( {} )
         .then(dbWorkout => {
             res.json(dbWorkout)
         })
         .catch(err => {
             res.json(err)
         })
-})
+});
 
-router.get("/api/workouts/range", ({ query }, res) => {
+router.get("/api/workouts/range", ( { query } , res) => {
     Workout.find({ day: { $gte: query.start, $lte: query.end } })
       .then(dbWorkouts => {
         res.json(dbWorkouts);
@@ -31,7 +31,7 @@ router.get("/api/workouts/range", ({ query }, res) => {
       });
   });
   
-  router.delete("/api/workouts", ({ body }, res) => {
+  router.delete("/api/workouts", ( { body } , res) => {
     Workout.findByIdAndDelete(body.id)
       .then(() => {
         res.json(true);
@@ -41,7 +41,7 @@ router.get("/api/workouts/range", ({ query }, res) => {
       });
   });
 
-  router.put("/api/workouts/:id", ({ body, params }, res) => {
+  router.put("/api/workouts/:id", ( { body, params } , res) => {
     Workout.findByIdAndUpdate(
       params.id,
       { $push: { exercises: body } },
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
-// Create express server
+
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -11,16 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Point to server routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
 
-// Set up DB
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-//   useNewUrlParser: true
+
 });
 
-// Start server
+
 app.listen(PORT, function() {
   console.log(`Now listening on port ${PORT} at http://localhost:${PORT}`);
 });
